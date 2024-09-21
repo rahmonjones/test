14cd36ae8f6d34872616a302c4ffd0216d97eef2
rahmonjones <rahmon.jay1@gmail.com>
2024-09-21 15:52:31 +0100

    main new

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "my_vpc" {      # Changed 'resources' to 'resource'
-  cidr_block = "10.0.0.0/24"       # Corrected the CIDR block
+resource "aws_vpc" "my_vpc" { # Changed 'resources' to 'resource'
+  cidr_block = "10.0.0.0/24"  # Corrected the CIDR block
 }
 
